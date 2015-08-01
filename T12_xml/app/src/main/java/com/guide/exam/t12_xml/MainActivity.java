acfdaba93001269e4c6fdc6d9ee99ff50d08277a
oraclejava7 <ab@ab.com>
2015-08-01 16:36:14 +0900

    xml

--- T12_xml/app/src/main/java/com/guide/exam/t12_xml/MainActivity.java
+++ T12_xml/app/src/main/java/com/guide/exam/t12_xml/MainActivity.java
@@ -44,6 +44,13 @@ public class MainActivity extends ActionBarActivity {
             return doc;
         }
 
+        private String getTagValue(Element dataElement, String tagName){
+            NodeList nodeList = dataElement.getElementsByTagName(tagName);
+            Element element = (Element)nodeList.item(0);// <day>0</day>
+            NodeList textNodeList = element.getChildNodes();
+            return textNodeList.item(0).getNodeValue();// 0
+        }
+
         @Override
         protected void onPostExecute(Document document) {
             super.onPostExecute(document);
@@ -52,24 +59,14 @@ public class MainActivity extends ActionBarActivity {
             NodeList nodeList = document.getElementsByTagName("data");
             for(int i=0; i<nodeList.getLength(); i++){
                 Element DataElement = (Element)nodeList.item(i);// <data>...</data>
+                str += " day : " + getTagValue(DataElement, "day");
+                str += " 시간 : " + getTagValue(DataElement, "hour");
+                str += " 온도 : " + getTagValue(DataElement, "temp");
+                str += " 날씨 : " + getTagValue(DataElement, "wfKor");
 
 
-                NodeList dayNodeList = DataElement.getElementsByTagName("day");
-                Element dayElement = (Element)dayNodeList.item(0);// <day>0</day>
-
-                NodeList dayTextNodeList = dayElement.getChildNodes();
-                str += "day : "+dayTextNodeList.item(0).getNodeValue();// 0
 
 
-                NodeList hourNodeList = DataElement.getElementsByTagName("hour");
-                Element hourElement = (Element)dayNodeList.item(0);// <hour>0</hour>
-
-                NodeList hourTextNodeList = dayElement.getChildNodes();
-                str += "hour : "+hourTextNodeList.item(0).getNodeValue();// 0
-
-                //
-                //
-                //
 
                 str += "\n";
             }
