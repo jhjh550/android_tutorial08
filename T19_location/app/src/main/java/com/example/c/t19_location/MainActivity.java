f49c8989ed5ebdfcf3752b8754078b0b5b489890
oraclejava <oravlejava>
2015-08-08 11:19:14 +0900

    location

---
+++ T19_location/app/src/main/java/com/example/c/t19_location/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.c.t19_location;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    String str;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LocationManager manager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        List<String> providers = manager.getAllProviders();
+
+        str="";
+        for(int i=0; i<providers.size(); i++){
+            str += providers.get(i) +" : " + manager.isProviderEnabled(providers.get(i))+"\n";
+        }
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                str += "\nlat : " + location.getLatitude();
+                str += " long : " + location.getLongitude();
+                str += " alt : " +  location.getAltitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10000,0.1f,locationListener);
+
+
+
+
+
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText(str);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
