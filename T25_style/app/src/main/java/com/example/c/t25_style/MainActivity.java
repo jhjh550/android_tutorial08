8f4359fc5766f4436a3c9730ad0e9b00a79ad2d3
oraclejava <oravlejava>
2015-08-09 17:28:45 +0900

    style

--- T25_style/app/src/main/java/com/example/c/t25_style/MainActivity.java
+++ T25_style/app/src/main/java/com/example/c/t25_style/MainActivity.java
@@ -1,16 +1,49 @@
 package com.example.c.t25_style;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView selectedTextView, workingTextView;
+    Button button1, button2, buttonEnter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        selectedTextView = (TextView)findViewById(R.id.selectedTextView);
+        workingTextView = (TextView)findViewById(R.id.workingTextView);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        buttonEnter = (Button)findViewById(R.id.enterButton);
+
+
+        View.OnClickListener numberButtonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b = (Button)v;
+                String str = workingTextView.getText().toString();
+                str += b.getText().toString();
+                workingTextView.setText(str);
+            }
+        };
+
+        button1.setOnClickListener(numberButtonListener);
+        button2.setOnClickListener(numberButtonListener);
+
+        buttonEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedTextView.setText(workingTextView.getText().toString());
+            }
+        });
     }
 
     @Override
