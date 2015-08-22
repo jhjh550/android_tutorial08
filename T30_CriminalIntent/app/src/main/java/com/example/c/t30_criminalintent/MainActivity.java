0cf6dedfe31221388ca88593fddb6dbce749e664
oraclejava <oravlejava>
2015-08-22 17:18:21 +0900

    criminalintent

--- T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/MainActivity.java
+++ T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.c.t30_criminalintent;
 
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+        }
     }
 
     @Override
