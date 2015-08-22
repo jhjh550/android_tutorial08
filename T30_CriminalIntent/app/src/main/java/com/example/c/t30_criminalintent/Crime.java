2e0e9f466d920b464602ffdd1eae9421c3a967af
oraclejava <oravlejava>
2015-08-22 17:28:05 +0900

    criminalintent

--- T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/Crime.java
+++ T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/Crime.java
@@ -1,16 +1,22 @@
 package com.example.c.t30_criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
  * Created by c on 2015-08-22.
  */
 public class Crime {
-    private String mTitle;
     private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+
 
     public Crime(){
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public String getTitle() {
@@ -27,5 +33,21 @@ public class Crime {
 
     public void setId(UUID id) {
         mId = id;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
