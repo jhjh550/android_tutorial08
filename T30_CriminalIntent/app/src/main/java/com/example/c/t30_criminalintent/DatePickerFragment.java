80a815d2dca7f9d1d012eeeb956dd32feea045d0
oraclejava <oravlejava>
2015-08-23 16:24:52 +0900

    criminalintent

--- T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/DatePickerFragment.java
+++ T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/DatePickerFragment.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.view.View;
 
 /**
  * Created by c on 2015-08-23.
@@ -13,7 +14,10 @@ public class DatePickerFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle("범죄 발생 일자")
                 .setPositiveButton("확인", null)
                 .create();
