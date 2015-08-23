3273d5a479f5e91a5b3d39c87709c50d7ae2919d
oraclejava <oravlejava>
2015-08-23 12:52:12 +0900

    criminalintent

--- T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/CrimeListFragment.java
+++ T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/CrimeListFragment.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -32,5 +35,27 @@ public class CrimeListFragment extends ListFragment{
     public void onListItemClick(ListView l, View v, int position, long id) {
         Crime c = (Crime)(getListAdapter()).getItem(position);
         Log.d("CrimeListFragment", c.getTitle()+" clicked");
+    }
+
+    private class CrimeAdapter extends ArrayAdapter<Crime>{
+        public CrimeAdapter(ArrayList<Crime> crimes){
+            super(getActivity(), 0, crimes);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null){
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_crime, null);
+            }
+            Crime c = getItem(position);
+            TextView titleTextView = (TextView)convertView.findViewById(R.id.crime_list_item_titleTextView);
+            titleTextView.setText(c.getTitle());
+            TextView dateTextView = (TextView)convertView.findViewById(R.id.crime_list_item_dateTextView);
+            dateTextView.setText(c.getDate().toString());
+            CheckBox solvedCheckBox = (CheckBox)convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+            solvedCheckBox.setChecked(c.isSolved());
+
+            return convertView;
+        }
     }
 }
