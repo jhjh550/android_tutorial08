6d004a06023bc2ea4f25f4fca3279a31a214c474
oraclejava <oravlejava>
2015-08-23 16:46:15 +0900

    criminalintent

--- T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/CrimeFragment.java
+++ T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/CrimeFragment.java
@@ -80,7 +80,8 @@ public class CrimeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 FragmentManager fm = getActivity().getSupportFragmentManager();
-                DatePickerFragment dialog = new DatePickerFragment();
+                //DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
                 dialog.show(fm, DIALOG_DATE);
             }
         });
--- T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/DatePickerFragment.java
+++ T30_CriminalIntent/app/src/main/java/com/example/c/t30_criminalintent/DatePickerFragment.java
@@ -7,10 +7,27 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
 
+import java.util.Date;
+
 /**
  * Created by c on 2015-08-23.
  */
 public class DatePickerFragment extends DialogFragment {
+    public static final String EXTRA_DATE = "criminalDate";
+
+    private Date mDate;
+    public static DatePickerFragment newInstance(Date date){
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_DATE, date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+
+
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
